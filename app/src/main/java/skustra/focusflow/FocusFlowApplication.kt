da4bc05edd908c839338c93a7c8170a7970a0342
s.kustra <skustra@mobitouch.net>
2023-04-04 16:49:27 +0200

    base dependencies

---
+++ app/src/main/java/skustra/focusflow/FocusFlowApplication.kt
@@ -0,0 +1,8 @@
+package skustra.focusflow
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FocusFlowApplication : Application() {
+}
--- app/src/main/java/skustra/focusflow/ui/activity/MainActivity.kt
+++ app/src/main/java/skustra/focusflow/ui/activity/MainActivity.kt
@@ -1,4 +1,4 @@
-package skustra.focusflow
+package skustra.focusflow.ui.activity
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -10,8 +10,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 import skustra.focusflow.ui.theme.FocusFlowTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
